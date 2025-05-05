3b7c4f101ae212f204dc993da318deec297a84b6
isaacingleby <isaacingleby@gmail.com>
2025-05-05 13:41:55 +0100

    Set up to define SystemSets, which can be used to control the order systems run in

--- src/player/mod.rs
+++ src/player/mod.rs
@@ -8,11 +8,24 @@ pub mod systems;
 const PLAYER_SIZE: f32 = 64.0; // Size of the player sprite
 pub const PLAYER_SIZE_HALF: f32 = PLAYER_SIZE / 2.0;
 
+#[derive(SystemSet, Hash, Debug, PartialEq, Eq, Clone)]
+pub struct MovementSystemSet;
+
+#[derive(SystemSet, Hash, Debug, PartialEq, Eq, Clone)]
+pub struct ConfinementSystemSet;
+
 pub struct PlayerPlugin;
 
 impl Plugin for PlayerPlugin {
     fn build(&self, app: &mut App) {
-        app.add_systems(Startup, spawn_player)
-            .add_systems(Update, (player_movement, confine_player_movement));
+        app.configure_sets(Update, MovementSystemSet.before(ConfinementSystemSet))
+            .add_systems(Startup, spawn_player)
+            .add_systems(
+                Update,
+                (
+                    player_movement.in_set(MovementSystemSet),
+                    confine_player_movement.in_set(ConfinementSystemSet),
+                ),
+            );
     }
 }
