12495b470053d8786a5f7db5e384333220102588
isaacingleby <isaacingleby@gmail.com>
2025-05-05 18:01:44 +0100

    Added redundant functions that are not used just incase they're needed later

--- src/game/systems.rs
+++ src/game/systems.rs
@@ -2,6 +2,16 @@ use bevy::prelude::*;
 
 use super::SimulationState;
 
+/// Added due to tutorial, but not used in my version of the game.
+pub fn _pause_simulation(mut next_simulation_state: ResMut<NextState<SimulationState>>) {
+    next_simulation_state.set(SimulationState::Paused);
+}
+
+/// Added due to tutorial, but not used in my version of the game.
+pub fn _resume_simulation(mut next_simulation_state: ResMut<NextState<SimulationState>>) {
+    next_simulation_state.set(SimulationState::Running);
+}
+
 pub fn toggle_simulation(
     keyboard_input: Res<ButtonInput<KeyCode>>,
     current_simulation_state: Res<State<SimulationState>>,
